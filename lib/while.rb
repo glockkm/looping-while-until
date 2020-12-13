e6f45d8bcd4a73f03b39a0e7bbf5c51e1ace24f1
Kimberly <kmglock@mail.lipscomb.edu>
2020-12-13 17:55:20 -0600

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,9 @@
-def using_while
-  levitation_force = 6
-	
-  #your code here
+def using_while # define method
+  levitation_force = 6 # define variable
+  while levitation_force < 10 # use while statement to repeat loop until variable count gets to 10
+    puts "Wingardium Leviosa"  # prints out
+    levitation_force += 1 # adds 1 to variable
+  end
 end
 
 
